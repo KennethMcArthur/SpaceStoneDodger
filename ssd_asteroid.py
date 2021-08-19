8fec0195cbfdc4ab97105932cc5bf8117c00d4d9
Kenneth <canowa@libero.it>
2021-08-19 09:17:57 +0200

    started working on a better asteroid management

--- ssd_asteroid.py
+++ ssd_asteroid.py
@@ -11,7 +11,12 @@ import ssd_constants as CST
 
 class Asteroid(pygame.sprite.Sprite):
     """ Asteroid Class: a single asteroid object """
+
+    asteroid_list = []
+
     def __init__(self, x: int, y: int, speed: int) -> None:
+        Asteroid.asteroid_list.append(self)
+        self.deathflag = False
         self.sprite_image = CST.ASTEROID_SPRITE
         self.radius = self.sprite_image.get_width() / 2
         self.height = self.sprite_image.get_height()
@@ -27,15 +32,36 @@ class Asteroid(pygame.sprite.Sprite):
         self.speed = speed
 
 
-    def game_tick_update(self, window: pygame.Surface) -> None:
+    def game_tick_update(self, window: pygame.Surface) -> bool:
+        if self.is_offscreen_left() and self.deathflag:
+            Asteroid.asteroid_list.remove(self)
+            return False
         self.x -= self.speed
         self.rect.x, self.rect.y = self.x, self.y
         window.blit(self.sprite_image, (self.x, self.y))
+        return True
 
 
     def is_offscreen_left(self) -> bool:
-        """ Checks if the asteroid is completely off screen to the left """
+        """ Used to know if the asteroid is out of screen """
         return self.x < (0 - self.width)
+
+
+    def mark_to_death(self) -> bool:
+        if self.deathflag == False:
+            self.deathflag = True
+            return True
+        else:
+            return False # already flagged for deletion
+
+
+    @staticmethod
+    def asteroids_still_alive() -> int:
+        """ returns the number of asteroids still not flagged for death """
+        return len([ast for ast in Asteroid.asteroid_list if ast.deathflag == False])
+
+   
+
 
 
 
@@ -51,50 +77,55 @@ class Field:
     y_offset = CST.ASTEROID_SPRITE.get_height() // 2
 
     def __init__(self, howmany: int, player) -> None:
-        self.size = howmany
         self.player = player
         self.min_speed = 3
         self.max_speed = 8
-        self.elements = [self.new_asteroid() for _ in range(self.size)]
-        self.to_be_deleted = 0
+        for _ in range(howmany):
+            self.new_asteroid()
 
+    def random_position(self):
+        newx = CST.SCREEN_WIDTH * 1.5 + randint(0, 100)
+        newy = randint(0-Field.y_offset , CST.SCREEN_HEIGHT-Field.y_offset)
+        newspeed = randint(self.min_speed, self.max_speed)
+        return newx, newy, newspeed
 
     def new_asteroid(self) -> Asteroid:
-        """ Internal method to create a new single asteroid to populate the Field """
-        return Asteroid(CST.SCREEN_WIDTH * 1.5 + randint(0, 100),
-                        randint(0-Field.y_offset , CST.SCREEN_HEIGHT-Field.y_offset),
-                        randint(self.min_speed, self.max_speed))
+        newx, newy, newspeed = self.random_position()
+        return Asteroid(newx, newy, newspeed)
 
 
     def resize(self, newsize: int) -> None:
-        """ Resizes the number of asteroids of the screen. The Field acts accordingly """
-        self.size = newsize
-        self.to_be_deleted = max(0, len(self.elements) - self.size) # Compressed if
-        # Adding new asteroids if size is greater
-        self.elements.extend([self.new_asteroid() for _ in range(self.size - len(self.elements))])
+        """ Resizes the number of asteroids of the screen """
+        oldsize = Asteroid.asteroids_still_alive() # ignoring death-flagged asteroids
+        if newsize < oldsize: # too much asteroids
+            flagged = 0
+            i = 0
+            while flagged < oldsize - newsize:
+                outcome = Asteroid.asteroid_list[i].mark_to_death()
+                flagged += outcome
+                i += outcome
+        elif oldsize < newsize: # need more asteroids
+            for _ in range(newsize - oldsize):
+                self.new_asteroid()
 
 
     def game_tick_update(self, window: pygame.Surface) -> None:
 
-        for element in self.elements[:]: # TRICK: iterating a COPY of the list allows safe resize of that list
-            if element.is_offscreen_left():
-                if self.to_be_deleted > 0: # we ditch this element if there are too many...
-                    self.elements.remove(element)
-                    self.to_be_deleted -= 1
-                    continue
-                else:
-                    newx = CST.SCREEN_WIDTH + randint(0, CST.SCREEN_WIDTH * 1.5)
-                    newy = randint(0-Field.y_offset, CST.SCREEN_HEIGHT-Field.y_offset)
-                    newspeed = randint(self.min_speed, self.max_speed)
-                    element.relocate(newx, newy, newspeed)
+        i = 0
+        while i < len(Asteroid.asteroid_list):
+            if Asteroid.asteroid_list[i].game_tick_update(window): # if update was succesful (asteroid still alive)
+                if Asteroid.asteroid_list[i].is_offscreen_left():
+                    newx, newy, newspeed = self.random_position()
+                    Asteroid.asteroid_list[i].relocate(newx, newy, newspeed)
 
-            element.game_tick_update(window)
+                # Collisions checking
+                if pygame.sprite.collide_circle(Asteroid.asteroid_list[i], self.player):
+                    pygame.event.post(pygame.event.Event(CST.PLAYER_HIT))
 
-            # Collisions checking
-            if pygame.sprite.collide_circle(element, self.player):
-                pygame.event.post(pygame.event.Event(CST.PLAYER_HIT))
-                
-        
+                i += 1
+
+
+
 
 
 
