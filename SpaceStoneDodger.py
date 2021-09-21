0e681aec18355b870785f56bf1cfcefe3d56fc16
Kenneth <canowa@libero.it>
2021-09-21 11:11:51 +0200

    added game tutorial scene, polished text and its position in other scenes

--- SpaceStoneDodger.py
+++ SpaceStoneDodger.py
@@ -20,31 +20,88 @@ pygame.init()
 
 
 
-
 class GameMenu(Scn.Scene):
     def scene_related_init(self):
+        SIZE_TEXT_BIG = 48
+        SIZE_TEXT_MEDIUM = 24
+        SIZE_TEXT_TINY = 14
         TITLE_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT * 0.2)
-        BOTTOM_TEXT_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT * 0.9)
-        KEYS_TEXT_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT * 0.7)
+        SUBTITLE_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT * 0.2 + 48)
+        BOTTOM_TEXT_COORDS_LEFT = (0, CST.SCREEN_HEIGHT - SIZE_TEXT_MEDIUM)
+        BOTTOM_TEXT_COORDS_RIGHT = (CST.SCREEN_WIDTH, CST.SCREEN_HEIGHT - SIZE_TEXT_MEDIUM)
 
         self.level_background = bg.Background()
         self.starfield = stf.Starfield(15)
-        self.player = plr.Player_pawn(CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT // 2)
-        self.text_title = txt.StaticText("Space Stone Dodger", 48, TITLE_COORDS, CST.TXT.CENTER)
-        self.text_keys = txt.StaticText("Move with W,A,S,D", 32, KEYS_TEXT_COORDS, CST.TXT.CENTER)
-        self.text_bottom = txt.StaticText("(press [SPACE] to begin)", 24, BOTTOM_TEXT_COORDS, CST.TXT.CENTER)
+        self.player = plr.Player_pawn(CST.SCREEN_WIDTH // 2 - 16, CST.SCREEN_HEIGHT // 2 - 16)
+        self.text_title = txt.StaticText("Space Stone Dodger", SIZE_TEXT_BIG, TITLE_COORDS, CST.TXT.CENTER)
+        self.text_subtitle = txt.StaticText("a simple game where you, well, dodge stones", SIZE_TEXT_TINY, SUBTITLE_COORDS, CST.TXT.CENTER)
+        self.text_goto_play = txt.StaticText("[P] Play", SIZE_TEXT_MEDIUM, BOTTOM_TEXT_COORDS_LEFT, CST.TXT.LEFT)
+        self.text_goto_tutorial = txt.StaticText("[T] Tutorial", SIZE_TEXT_MEDIUM, BOTTOM_TEXT_COORDS_RIGHT, CST.TXT.RIGHT)
         
         # Append order is draw order
         self.updatelist.append(self.level_background)
         self.updatelist.append(self.starfield)
         self.updatelist.append(self.player)
         self.updatelist.append(self.text_title)
-        self.updatelist.append(self.text_keys)
-        self.updatelist.append(self.text_bottom)
+        self.updatelist.append(self.text_subtitle)
+        self.updatelist.append(self.text_goto_play)
+        self.updatelist.append(self.text_goto_tutorial)
+
 
     def keys_to_check(self, key_list: list):
         self.player.handle_movement(key_list)
-        if CST.pressed("SPACE", key_list):
+        if CST.pressed("P", key_list):
+            self.quit_loop(CST.SCENES.GAME_LEVEL)
+        if CST.pressed("T", key_list):
+            self.quit_loop(CST.SCENES.GAME_TUTORIAL)
+
+
+
+class GameTutorial(Scn.Scene):
+    def scene_related_init(self):
+        SIZE_TEXT_MEDIUM = 28
+        SIZE_TEXT_SMALL = 18
+        SIZE_TEXT_TINY = 14
+        FIRST_COL = 50
+        SECOND_COL = 150
+        FIRST_ROW = CST.SCREEN_HEIGHT // 4 * 1
+        SECOND_ROW = CST.SCREEN_HEIGHT // 4 * 2
+        THIRD_ROW = CST.SCREEN_HEIGHT // 4 * 3
+        BOTTOM_ROW = CST.SCREEN_HEIGHT - SIZE_TEXT_SMALL
+
+        self.level_background = bg.Background()
+        self.starfield = stf.Starfield(15)
+        self.player = plr.Player_pawn(FIRST_COL, FIRST_ROW)
+        self.player_life_bar = plr.Lifebar(self.player)
+        self.asteroid = ast.Asteroid(FIRST_COL, SECOND_ROW, 0)
+        self.powerup = pwr.PowerUp(FIRST_COL, THIRD_ROW, 0)
+        self.player_label = txt.StaticText("Move with W,A,S,D", SIZE_TEXT_MEDIUM, (SECOND_COL, FIRST_ROW))
+        self.asteroid_label = txt.StaticText("Avoid asteroids", SIZE_TEXT_MEDIUM, (SECOND_COL, SECOND_ROW))
+        self.powerup_label = txt.StaticText("Collect valuable scraps", SIZE_TEXT_MEDIUM, (SECOND_COL, THIRD_ROW))
+        self.player_life_bar_label = txt.StaticText("Your life", SIZE_TEXT_TINY, (CST.SCREEN_WIDTH, 50), CST.TXT.RIGHT)
+        self.goto_menu_label = txt.StaticText("[M] back to Menu", SIZE_TEXT_SMALL, (0, BOTTOM_ROW), CST.TXT.LEFT)
+        self.goto_play_label = txt.StaticText("[P] Play the game", SIZE_TEXT_SMALL, (CST.SCREEN_WIDTH, BOTTOM_ROW), CST.TXT.RIGHT)
+
+        # Append order is draw order
+        self.updatelist.append(self.level_background)
+        self.updatelist.append(self.starfield)
+        self.updatelist.append(self.player)
+        self.updatelist.append(self.player_life_bar)
+        self.updatelist.append(self.asteroid)
+        self.updatelist.append(self.powerup)
+        self.updatelist.append(self.player_label)
+        self.updatelist.append(self.asteroid_label)
+        self.updatelist.append(self.powerup_label)
+        self.updatelist.append(self.player_life_bar_label)
+        self.updatelist.append(self.goto_menu_label)
+        self.updatelist.append(self.goto_play_label)
+
+
+    def keys_to_check(self, key_list: list) -> None:
+        self.player.handle_movement(key_list)
+        if CST.pressed("M", key_list):
+            self.quit_loop(CST.SCENES.GAME_MENU)
+        if CST.pressed("P", key_list):
             self.quit_loop(CST.SCENES.GAME_LEVEL)
 
 
@@ -91,19 +148,25 @@ class GameLevel(Scn.Scene):
 
 class GameLosingScreen(Scn.Scene):
     def scene_related_init(self):
+        SIZE_TEXT_BIG = 48
+        SIZE_TEXT_SMALL = 18
         TITLE_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT // 2)
-        BOTTOM_TEXT_COORDS = (CST.SCREEN_WIDTH // 2, CST.SCREEN_HEIGHT * 0.9)
+        BOTTOM_ROW = CST.SCREEN_HEIGHT - SIZE_TEXT_SMALL
 
         self.level_background = bg.Background()
-        self.text_title = txt.StaticText("Sadly, stones Won", 48, TITLE_COORDS, CST.TXT.CENTER)
-        self.text_bottom = txt.StaticText("(press [SPACE] to play again)", 20, BOTTOM_TEXT_COORDS, CST.TXT.CENTER)
+        self.text_title = txt.StaticText("Sadly, stones Won", SIZE_TEXT_BIG, TITLE_COORDS, CST.TXT.CENTER)
+        self.goto_menu_label = txt.StaticText("[M] back to Menu", SIZE_TEXT_SMALL, (0, BOTTOM_ROW), CST.TXT.LEFT)
+        self.goto_play_label = txt.StaticText("[P] Play again", SIZE_TEXT_SMALL, (CST.SCREEN_WIDTH, BOTTOM_ROW), CST.TXT.RIGHT)
         
         self.updatelist.append(self.level_background)
         self.updatelist.append(self.text_title)
-        self.updatelist.append(self.text_bottom)
+        self.updatelist.append(self.goto_menu_label)
+        self.updatelist.append(self.goto_play_label)
 
     def keys_to_check(self, key_list: list):
-        if CST.pressed("SPACE", key_list):
+        if CST.pressed("M", key_list):
+            self.quit_loop(CST.SCENES.GAME_MENU)
+        if CST.pressed("P", key_list):
             self.quit_loop(CST.SCENES.GAME_LEVEL)
   
 
@@ -116,16 +179,18 @@ def main_game():
     pygame.display.set_caption("Space Stone Dodger!")
 
     game_menu = GameMenu(WIN)
+    game_tutorial = GameTutorial(WIN)
     game_level = GameLevel(WIN)
     game_losing_screen = GameLosingScreen(WIN)
 
     scenelist = [
         game_menu,
+        game_tutorial,
         game_level,
         game_losing_screen,
     ]
 
-    next_scene = 0
+    next_scene = CST.SCENES.GAME_MENU
 
     # Scene sequence, each scene returns the index for the next one
     while True:
